ef5311cd853533ff9f3e6b6b3b28e7afc6f9b2f4
Marcio R S Martins <99811881+marciomartins83@users.noreply.github.com>
2025-10-15 18:21:43 +0000

    feat: adicionar suporte a autenticação e proteção CSRF

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,27 +1,29 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
+    public function login(
+        AuthenticationUtils $authenticationUtils,
+        CsrfTokenManagerInterface $csrfTokenManager
+    ): Response {
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        $csrfToken = $csrfTokenManager->getToken('authenticate')->getValue();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'csrf_token' => $csrfToken,
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
